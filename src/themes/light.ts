2dfe919195ac467087a665f7224be2515346a178
Kevin Willian <keviwillian084@gmail.com>
2023-04-12 23:51:08 -0300

    feat: add light mode theme

---
+++ src/themes/light.ts
@@ -0,0 +1,19 @@
+export const light = {
+  colors: {
+    primary: '#FCC636',
+    secondary: '#2B2B2B',
+    tertiary: '#FFFFFF',
+
+    background: '#1F1F1F',
+    text: '#FFFFFF',
+
+    black: '#000000',
+    white: '#FFFFFF',
+    gray: '#BFBFBF',
+  },
+
+  fonts: {
+    regular: 'Poppins_400Regular',
+    bold: 'Poppins_700Bold',
+  },
+};
