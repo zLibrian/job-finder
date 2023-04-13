147a293595269175c7690bdb7fc15829b4a97720
Kevin Willian <keviwillian084@gmail.com>
2023-04-12 23:21:46 -0300

    feat: add roboto font to the app

--- App.tsx
+++ App.tsx
@@ -1,7 +1,22 @@
+import {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto';
 import { NavigationContainer } from '@react-navigation/native';
+
 import Routes from './src/routes';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) return null;
+
   return (
     <NavigationContainer>
       <Routes />
