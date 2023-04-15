b85d1abe94602d4da908709fcdf3394c79797e01
Kevin Willian <keviwillian084@gmail.com>
2023-04-15 00:27:58 -0300

    feat: :sparkles: finish the Header styles and remove the Header component

--- src/components/Header/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Text, View } from 'react-native';
-export function Header() {
-  return (
-    <View>
-      <Text>Header</Text>
-    </View>
-  );
-}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Image } from 'react-native';
 import Home from '../screens/Home';
 import JobDetails from '../screens/JobDetails';
 
@@ -11,9 +12,33 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function Routes() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="JobDetails" component={JobDetails} />
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#1F1F1F',
+        },
+        headerTitleStyle: {
+          fontFamily: 'Roboto_400Regular',
+        },
+        headerTintColor: '#fff',
+        headerRight: () => (
+          <Image source={require('../assets/userAvatar.png')} />
+        ),
+      }}>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerTitle: 'Hi There 👋',
+        }}
+      />
+      <Stack.Screen
+        name="JobDetails"
+        component={JobDetails}
+        options={{
+          headerTitle: 'Job Details',
+        }}
+      />
     </Stack.Navigator>
   );
 }
