15f275792412185f25c29c78f2174b24d17852f7
Kevin Willian <keviwillian084@gmail.com>
2023-04-15 21:03:08 -0300

    feat: :lipstick: add styles to the Button component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,35 @@
+import styled, { css } from 'styled-components/native';
+import { CustomText } from '../../typography/CustomText';
+
+import { TouchableOpacityProps } from 'react-native';
+
+export type ButtonProps = TouchableOpacityProps & {
+  title: string;
+  RightIcon?: React.ReactNode;
+  LeftIcon?: React.ReactNode;
+};
+
+export const ButtonContainer = styled.TouchableOpacity<ButtonProps>`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  width: 115px;
+  height: 46px;
+
+  border: 1px solid #4e4e4e;
+  border-radius: 25px;
+  background-color: ${({ theme }) => theme.colors.secondary};
+
+  ${({ RightIcon, LeftIcon }) =>
+    (RightIcon || LeftIcon) &&
+    css`
+      width: auto;
+      height: auto;
+      padding: 7px 14px;
+    `}
+`;
+
+export const ButtonText = styled(CustomText)`
+  padding: 0 4px;
+`;
