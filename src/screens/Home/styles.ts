4b2306b68072cbb9169e91599f780990a77d6617
Kevin Willian <keviwillian084@gmail.com>
2023-04-16 00:42:24 -0300

    feat: :lipstick: add a gap to the main container of Home screen

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components/native';
 export const Container = styled.SafeAreaView`
   flex: 1;
   align-items: center;
+  gap: 24px;
 
   background-color: ${({ theme }) => theme.colors.background};
   padding: 16px;
