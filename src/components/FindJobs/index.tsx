6990e627ad03421ccb109c3a3f79787de7aa14b0
Kevin Willian <keviwillian084@gmail.com>
2023-04-16 00:41:59 -0300

    feat: :sparkles: add the SearchArea component to the FindJobs component

--- src/components/FindJobs/index.tsx
+++ src/components/FindJobs/index.tsx
@@ -1,5 +1,7 @@
 import { CustomText } from '../../typography/CustomText';
 import { Button } from '../Button';
+import { SearchArea } from './SearchArea';
+
 import { Container, FindJobsContainer } from './styles';
 
 export function FindJobs() {
@@ -9,6 +11,8 @@ export function FindJobs() {
         <CustomText fontSize="32px">Find Jobs</CustomText>
         <Button title="Saved Jobs" />
       </FindJobsContainer>
+
+      <SearchArea />
     </Container>
   );
 }
--- src/components/FindJobs/styles.ts
+++ src/components/FindJobs/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   width: 100%;
+  gap: 16px;
 `;
 
 export const FindJobsContainer = styled.View`
