c98ce9fefa59651c538c3403914e277831a3699d
Kevin Willian <keviwillian084@gmail.com>
2023-04-15 21:04:09 -0300

    feat: :lipstick: add styles to the FindJobs component

---
+++ src/components/FindJobs/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 100%;
+`;
+
+export const FindJobsContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
